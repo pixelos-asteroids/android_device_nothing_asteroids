b55919b58e97415e9bec53d5430cb946818b7c12
NayanChavare <nayanjinal23@gmail.com>
2026-09-26 12:53:54 +0530

    asteroids: Align early boot display density detection with stock Nothing OS
    Import Nothing OS's framebuffer width threshold in set_density_by_fb(),
    correctly assigning vendor.display.lcd_density to 420 for 1080p displays
    instead of the generic CAF 480 fallback. This ensures runtime display
    density matches TARGET_SCREEN_DENSITY := 420 in BoardConfig.mk.

--- init/init.qcom.early_boot.sh
+++ init/init.qcom.early_boot.sh
@@ -77,8 +77,10 @@ function set_density_by_fb() {
            setprop vendor.display.lcd_density 640
         elif [ $fb_width -ge 1440 ]; then
            setprop vendor.display.lcd_density 560
-        elif [ $fb_width -ge 1080 ]; then
+        elif [ $fb_width -ge 1200 ]; then
            setprop vendor.display.lcd_density 480
+        elif [ $fb_width -ge 1080 ]; then
+           setprop vendor.display.lcd_density 420
         elif [ $fb_width -ge 720 ]; then
            setprop vendor.display.lcd_density 320 #for 720X1280 resolution
         elif [ $fb_width -ge 480 ]; then
